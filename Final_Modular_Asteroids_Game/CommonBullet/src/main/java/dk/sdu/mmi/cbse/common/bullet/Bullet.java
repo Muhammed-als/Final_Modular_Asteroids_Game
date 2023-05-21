94725c9905f278757c3ea5f88ea75c3f3aaf75b2
Muhammed Alsabban <muals21@student.sdu.dk>
2023-05-21 08:18:03 +0200

    Change directory name to Final_Modular_Asteroids_Game

--- Final_Modular_Asteroids_Game/CommonBullet/src/main/java/dk/sdu/mmi/cbse/common/bullet/Bullet.java
+++
@@ -1,5 +0,0 @@
-package dk.sdu.mmi.cbse.common.bullet;
-//
-import dk.sdu.mmmi.cbse.common.data.Entity;
-public class Bullet extends Entity {
-}
--- Final_Modular_Asteroids_Game/CommonBullet/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module CommonBullet {
-    exports dk.sdu.mmi.cbse.common.bullet;
-    requires Common;
-    uses dk.sdu.mmi.cbse.common.bullet.Bullet;
-    uses dk.sdu.mmi.cbse.common.bullet.BulletSPI;
-
-
-}
