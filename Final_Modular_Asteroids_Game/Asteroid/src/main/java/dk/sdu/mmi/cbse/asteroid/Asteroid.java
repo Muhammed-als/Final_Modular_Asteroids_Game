5342a719be17354ab2de3a0c78d1b93322b37e5d
Muhammed Alsabban <muals21@student.sdu.dk>
2023-05-23 07:20:05 +0200

    Refactored the name of the differnet components

--- Final_Modular_Asteroids_Game/Asteroid/src/main/java/dk/sdu/mmi/cbse/asteroid/Asteroid.java
+++
@@ -1,6 +0,0 @@
-package dk.sdu.mmi.cbse.asteroid;
-
-import dk.sdu.mmmi.cbse.common.data.Entity;
-
-public class Asteroid extends Entity {
-}
--- Final_Modular_Asteroids_Game/Enemy/src/main/java/module-info.java
+++ Final_Modular_Asteroids_Game/Enemy/src/main/java/module-info.java
@@ -1,9 +1,12 @@
+import dk.sdu.mmmi.cbse.enemysystem.EnemyControlSystem;
+import dk.sdu.mmmi.cbse.enemysystem.EnemyPlugin;
+
 module Enemy {
     requires Common;
     requires com.badlogic.gdx;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
-    provides dk.sdu.mmmi.cbse.common.services.IEntityProcessingService with dk.sdu.mmi.cbse.enemysystem.EnemyControlSystem;
-    provides dk.sdu.mmmi.cbse.common.services.IGamePluginService with dk.sdu.mmi.cbse.enemysystem.EnemyPlugin;
+    provides dk.sdu.mmmi.cbse.common.services.IEntityProcessingService with EnemyControlSystem;
+    provides dk.sdu.mmmi.cbse.common.services.IGamePluginService with EnemyPlugin;
 }
